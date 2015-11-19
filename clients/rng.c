96c168656cd8df7ae873783cc28991ef531862e8
Sol Boucher <sboucher@cmu.edu>
2015-11-19 02:01:25 -0500

    clients/rng: Add second constructor permitting user-specified increments

--- clients/rng.c
+++ clients/rng.c
@@ -23,9 +23,23 @@ static inline bool coprime(unsigned a, size_t bnfacts, const int *bpfacts) {
 
 // Picks parameters compliant with Hull-Dobell Thm. to achieve full cycle
 rng_t *rng_lcfc_init(unsigned period, size_t nfactors, const int *pfactors) {
+	unsigned incr;
+	do {
+		do
+			incr = rand() % period;
+		while(incr == 0 || incr % period == 1 || incr % period == period - 1);
+	} while(!coprime(incr, nfactors, pfactors));
+	return rng_lcfc_init_incr(period, nfactors, pfactors, incr);
+}
+
+// Ensures parameters are compliant with Hull-Dobell Thm.
+rng_t *rng_lcfc_init_incr(unsigned period, size_t nfactors, const int *pfactors, unsigned incr) {
 	assert(period >= 5);
 	assert(nfactors);
 	assert(pfactors);
+
+	if(!coprime(incr, nfactors, pfactors))
+		return NULL;
 
 	static bool seeded = false;
 	if(!seeded)
@@ -42,12 +56,7 @@ rng_t *rng_lcfc_init(unsigned period, size_t nfactors, const int *pfactors) {
 		res->coeff *= pfactors[idx];
 	res->coeff += 1;
 
-	do {
-		do
-			res->incr = rand() % period;
-		while(res->incr == 0 || res->incr % period == 1 ||
-				res->incr % period == period - 1);
-	} while(!coprime(res->incr, nfactors, pfactors));
+	res->incr = incr;
 
 	res->mod = period;
 
--- clients/rng.h
+++ clients/rng.h
@@ -8,6 +8,9 @@ typedef struct rng_state rng_t;
 // pfactors must contain all prime factors of period, which must be at least 5
 rng_t *rng_lcfc_init(unsigned period, size_t nfactors, const int *pfactors);
 
+// incr must be coprime with period
+rng_t *rng_lcfc_init_incr(unsigned period, size_t nfactors, const int *pfactors, unsigned incr);
+
 void rng_lcfc_clean(rng_t *victim);
 
 unsigned rng_lcfc(rng_t *ctxt);
