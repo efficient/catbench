9053201a9aa5d4d18de3a723b7ffe419cf6b3b89
Sol Boucher <sboucher@cmu.edu>
2015-12-08 02:47:43 -0500

    Add syscallers header wrapping syscalls and const-incorrect library functions
    It currently contains perf_event_open() and an execv() wrapper.

---
+++ syscallers.h
@@ -0,0 +1,19 @@
+#ifndef PERF_H_
+#define PERF_H_
+
+#include <asm/unistd.h>
+#include <linux/perf_event.h>
+#include <unistd.h>
+
+static inline int perf_event_open(struct perf_event_attr *attr, pid_t pid, int cpu, int group_fd,
+		unsigned long flags) {
+	return syscall(__NR_perf_event_open, attr, pid, cpu, group_fd, flags);
+}
+
+static inline int exec_v(const char *path, const char *const argv[]) {
+	#pragma GCC diagnostic ignored "-Wcast-qual"
+	return execv(path, (char *const *) argv);
+	#pragma GCC diagnostic pop
+}
+
+#endif
