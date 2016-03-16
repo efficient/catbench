3f370bf54d9e453d1ff19ac10f21bdc3fbdc58c5
Thomas Kim <thomask@cs.cmu.edu>
2016-03-16 15:18:46 -0400

    clients/square_evictions: Changed square evictions to do reads only read-only-unrolled*: benchmarks

--- clients/square_evictions.c
+++ clients/square_evictions.c
@@ -220,6 +220,7 @@ static int square_evictions(uint8_t *arr, int cache_line_size, int num_periods,
 	}
 		
 	clock_t duration = 0;
+	volatile int count = 0;
 	for(int cycle = 0; cycle < 2 * num_periods; ++cycle) {
 		const char *desc = "";
 		rng_t *randomizer = randomize;
@@ -281,113 +282,37 @@ static int square_evictions(uint8_t *arr, int cache_line_size, int num_periods,
 					for(int extra = subaccesses - 1; extra >= MAX_TRUE_UNROLL; --extra)
 						arr[ix[extra]] ^= val[extra];
 				case 16:
-					arr[ix[15]] ^= val[15];
+					count += arr[ix[15]];
 				case 15:
-					arr[ix[14]] ^= val[14];
+					count += arr[ix[14]];
 				case 14:
-					arr[ix[13]] ^= val[13];
+					count += arr[ix[13]];
 				case 13:
-					arr[ix[12]] ^= val[12];
+					count += arr[ix[12]];
 				case 12:
-					arr[ix[11]] ^= val[11];
+					count += arr[ix[11]];
 				case 11:
-					arr[ix[10]] ^= val[10];
+					count += arr[ix[10]];
 				case 10:
-					arr[ix[9]] ^= val[9];
+					count += arr[ix[9]];
 				case 9:
-					arr[ix[8]] ^= val[8];
+					count += arr[ix[8]];
 				case 8:
-					arr[ix[7]] ^= val[7];
+					count += arr[ix[7]];
 				case 7:
-					arr[ix[6]] ^= val[6];
+					count += arr[ix[6]];
 				case 6:
-					arr[ix[5]] ^= val[5];
+					count += arr[ix[5]];
 				case 5:
-					arr[ix[4]] ^= val[4];
+					count += arr[ix[4]];
 				case 4:
-					arr[ix[3]] ^= val[3];
+					count += arr[ix[3]];
 				case 3:
-					arr[ix[2]] ^= val[2];
+					count += arr[ix[2]];
 				case 2:
-					arr[ix[1]] ^= val[1];
+					count += arr[ix[1]];
 				case 1:
-					arr[ix[0]] ^= val[0];
-				};
-
-				switch(subaccesses) {
-				default:
-					for(int extra = subaccesses - 1; extra >= MAX_TRUE_UNROLL; --extra)
-						val[extra] ^= arr[ix[extra]];
-				case 16:
-					val[15] ^= arr[ix[15]];
-				case 15:
-					val[14] ^= arr[ix[14]];
-				case 14:
-					val[13] ^= arr[ix[13]];
-				case 13:
-					val[12] ^= arr[ix[12]];
-				case 12:
-					val[11] ^= arr[ix[11]];
-				case 11:
-					val[10] ^= arr[ix[10]];
-				case 10:
-					val[9] ^= arr[ix[9]];
-				case 9:
-					val[8] ^= arr[ix[8]];
-				case 8:
-					val[7] ^= arr[ix[7]];
-				case 7:
-					val[6] ^= arr[ix[6]];
-				case 6:
-					val[5] ^= arr[ix[5]];
-				case 5:
-					val[4] ^= arr[ix[4]];
-				case 4:
-					val[3] ^= arr[ix[3]];
-				case 3:
-					val[2] ^= arr[ix[2]];
-				case 2:
-					val[1] ^= arr[ix[1]];
-				case 1:
-					val[0] ^= arr[ix[0]];
-				};
-
-				switch(subaccesses) {
-				default:
-					for(int extra = subaccesses - 1; extra >= MAX_TRUE_UNROLL; --extra)
-						arr[ix[extra]] ^= val[extra];
-				case 16:
-					arr[ix[15]] ^= val[15];
-				case 15:
-					arr[ix[14]] ^= val[14];
-				case 14:
-					arr[ix[13]] ^= val[13];
-				case 13:
-					arr[ix[12]] ^= val[12];
-				case 12:
-					arr[ix[11]] ^= val[11];
-				case 11:
-					arr[ix[10]] ^= val[10];
-				case 10:
-					arr[ix[9]] ^= val[9];
-				case 9:
-					arr[ix[8]] ^= val[8];
-				case 8:
-					arr[ix[7]] ^= val[7];
-				case 7:
-					arr[ix[6]] ^= val[6];
-				case 6:
-					arr[ix[5]] ^= val[5];
-				case 5:
-					arr[ix[4]] ^= val[4];
-				case 4:
-					arr[ix[3]] ^= val[3];
-				case 3:
-					arr[ix[2]] ^= val[2];
-				case 2:
-					arr[ix[1]] ^= val[1];
-				case 1:
-					arr[ix[0]] ^= val[0];
+					count += arr[ix[0]];
 				};
 
 				if(measure_all) {
@@ -436,7 +361,6 @@ breakoutermost:
 
 	if(*perflog && !perf_poll_stop(perfd))
 		perror("Stopping performance recording");
-
 	return 0;
 }
 
