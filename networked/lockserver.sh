f5131ff41e168169f988f853d969591d286ca8c5
Sol Boucher <sboucher@cmu.edu>
2016-07-21 17:15:41 -0400

    networked/lockserver.sh: Use Mica-inspired server initialization waits to resolve timeouts
    Before this, many of the points were failing due to timeouts in very early samples.

--- networked/lockserver.sh
+++ networked/lockserver.sh
@@ -4,7 +4,7 @@ CLIENT_BIN="lockclient"
 PERF_INIT_PHRASE="Initialization complete!"
 
 inherit_default_init="SERVER_DIR CLIENT_DIR CONTENDER_DIR CONTENDER_BIN"
-inherit_default_impl="gencontenderargs prephugepages awaitserverinit waitbeforeclient extractavelatency extractalllatencies extracttaillatency"
+inherit_default_impl="gencontenderargs prephugepages extractavelatency extractalllatencies extracttaillatency"
 
 genserverargs() {
 	echo "-l '$entries' -- -c 0x1"
@@ -12,6 +12,15 @@ genserverargs() {
 
 genclientargs() {
 	echo "-m '$local_macaddr' -s '$client_sleep'"
+}
+
+awaitserverinit() {
+	waitforalloc 0
+	sleep 3
+}
+
+waitbeforeclient() {
+	while ! grep "Initialization complete" >/dev/null 2>&1; do sleep 1; done
 }
 
 extracttput() {
