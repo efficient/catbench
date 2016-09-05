63c92371659a98af1f3d0208f9dcf46e05538cfe
Sol Boucher <sboucher@cmu.edu>
2016-09-05 16:21:55 -0400

    networked/contender_duration.sh: Warn if MAIN_DURATION is left to the default
    This constant must be specified with the contender_duration operation mode; otherwise, the results
    for the baseline and basealloc series will not be comparable to those from contention and
    allocation.

--- networked/contender_duration.sh
+++ networked/contender_duration.sh
@@ -1,7 +1,7 @@
 SPAWNCONTENDERS="onmainprocessing"
 
 const WARMUP_DURATION="3s"
-const MAIN_DURATION="10s"
+const warn MAIN_DURATION="10s"
 
 inherit_default_init="$inherit_default_init"
 inherit_default_impl="$inherit_default_impl"
