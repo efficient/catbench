22538d6e91f6ac73022f05526b998e3823c26965
Hyeontaek Lim <hl@cs.cmu.edu>
2016-09-09 18:50:13 -0400

    find_zipf_N.py: Fix typos

--- find_zipf_N.py
+++ find_zipf_N.py
@@ -14,7 +14,7 @@ def find_zipf_N(cache_size, zipf_alpha, percentile):
 
     # Find N such that
     # percentile = sum_{i=1}{cache_item_count}{1/i**zipf_alpha} /
-    #              sum_{i=1}{N}{1/i**zipf_alpha} /
+    #              sum_{i=1}{N}{1/i**zipf_alpha}
 
     numerator = 0.
     for i in range(1, cache_item_count + 1):
@@ -48,7 +48,7 @@ def test_find_zipf_N():
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print('%s CACHE_SIZE ZIPF-ALPHA PERCENTILE' % sys.argv[0])
+        print('%s CACHE-SIZE ZIPF-ALPHA PERCENTILE' % sys.argv[0])
         sys.exit(1)
     cache_size = int(sys.argv[1])
     zipf_alpha = float(sys.argv[2])
